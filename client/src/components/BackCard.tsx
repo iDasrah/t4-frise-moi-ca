ea76bb64ed9378ca9156be7aa352281900a01cc8
Mathéo Picouleau <matheo.picouleau@etu.unistra.fr>
2025-04-09 16:05:39 +0200

    style backcard

--- client/src/components/BackCard.tsx
+++ client/src/components/BackCard.tsx
@@ -6,6 +6,16 @@ const BackCard = ({ onClick } : BackCardProps) => {
     return (
         <>
             <div className="card back" onClick={onClick}>
+                <div className="card-header">
+                    Thématique
+                </div>
+
+                <div className="w-full h-full p-1">
+                    <div className="flex flex-col h-full justify-between items-center bg-darkRed p-4 text-cream rounded-xl">
+                        <p className="text-center">Titre</p>
+                        <img src="/t4_card_bg.png" className="w-24"  alt="Logo"/>
+                    </div>
+                </div>
             </div>
         </>
     )
