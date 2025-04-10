af499eda3ba4dfb0b735d0092e96d466582f379e
lucien.schwob <lucien.schwob@etu.unistra.fr>
2025-04-10 12:19:23 +0200

    chore: ajout des types utilisateurs et jeu

--- client/src/types.ts
+++ client/src/types.ts
@@ -10,3 +10,17 @@ export enum CardSide {
     FRONT = 'front',
     BACK = 'back',
 }
+
+export type User = {
+    id: string;
+    name: string;
+    gameCode: string;
+    isHost: boolean;
+    points: number;
+}
+
+export type Game = {
+    code: string;
+    hasStarted: boolean;
+    maxPlayers: number;
+}
