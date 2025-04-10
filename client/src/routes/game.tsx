170bb4af990a7762de7b00dd9230a681559a9fe9
lucien.schwob <lucien.schwob@etu.unistra.fr>
2025-04-10 14:24:08 +0200

    fix: retrait du props de game

--- client/src/routes/game.tsx
+++ client/src/routes/game.tsx
@@ -1,18 +1,12 @@
 import {useParams} from "react-router";
-import { CardData } from "../types.ts";
 import GameBoard from "../components/GameBoard.tsx";
 
-interface GameProps {
-    data: CardData[];
-}
-
-const Game = ({ data }: GameProps) => {
+const Game = () => {
     const params = useParams<{ game_code: string }>();
     const gameCode = params.game_code;
 
     return (
         <div>
-            <GameBoard data={data} />
         </div>
     )
 }
