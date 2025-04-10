f83bcf327fda7f1163c9c19d5c11e7f449ac0f71
LAMORLETTE MAXIME <maxime.lamorlette@etu.unistra.fr>
2025-04-10 10:16:18 +0200

    ajout de la waiting room correctement

--- client/src/routes/waiting-room.tsx
+++ client/src/routes/waiting-room.tsx
@@ -8,7 +8,9 @@ return (
         <div className="w-full h-full flex flex-row gap-8 p-8 bg-gray-700 rounded-xl shadow-lg">
             {/* Carte Joueurs */}
             <div className="bg-gray-300 text-black p-6 rounded-lg border-2 border-black w-1/4">
-                <h3 className="font-bold text-xl mb-3">Joueurs 3/4</h3>
+                <div className="h-10 border border-black rounded-md flex items-center justify-center text-lg">
+                <h3 className="italic font-bold text-xl mb-3 text-center">Joueurs 3/4</h3>
+                    </div>
                 <ul className="italic space-y-2 text-lg">
                     <li>Joueur 1</li>
                     <li>Joueur 2</li>
@@ -20,7 +22,7 @@ return (
             <div className="bg-gray-300 text-black p-6 rounded-lg flex flex-col justify-start w-3/4">
                 {/* Champ réglages visuel */}
                 <div className="h-10 border border-black rounded-md flex items-center justify-center text-lg">
-                    <span className="italic font-semibold">Réglages :</span>
+                    <span className="italic font-bold text-xl mb-3 text-center">Réglages :</span>
                 </div>
                 {/* Code room */}
                 <div className="italic font-semibold pt-4 text-center text-lg">
