3a8e0744043b21d63c8f06d50f2a667ee215ac07
Mathéo Picouleau <matheo.picouleau@etu.unistra.fr>
2025-04-11 06:19:19 +0200

    config vite pour vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    base: "/",
+    build: {
+        outDir: "dist",
+    },
+    plugins: [react(), tailwindcss()],
 })
