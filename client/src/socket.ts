0c4e73567a99ef36ae788583c305677e593d74b0
Mathéo Picouleau <matheo.picouleau@etu.unistra.fr>
2025-04-11 05:36:49 +0200

    utilisation vite env

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
+const socketUrl = import.meta.env.VITE_SOCKET_URL;
 
-export const socket = io(process.env.API_URL, {
+export const socket = io(socketUrl, {
     autoConnect: false,
 });
