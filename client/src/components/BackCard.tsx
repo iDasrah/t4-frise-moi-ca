f5728db076c9be64cbf0e7c02bf66f123f48893a
Mathéo Picouleau <matheo.picouleau@etu.unistra.fr>
2025-04-11 09:59:59 +0200

    affichage carte pioche

--- client/src/components/BackCard.tsx
+++ client/src/components/BackCard.tsx
@@ -1,21 +1,13 @@
-import { CardData } from "../types.ts";
-
-interface BackCardProps {
-    onClick: () => void;
-    data?: Omit<CardData, "type" | "description" | "date">;
-}
-
-const BackCard = ({ data }: BackCardProps) => {
+const BackCard = () => {
     return (
         <div
             className="card back w-full h-full rounded-lg overflow-hidden cursor-pointer"
         >
-            <div className="card-header">{data?.thematic}</div>
+            <div className="card-header">Frise-moi ça !</div>
             <hr className="border-3 border-cream2 w-full"></hr>
 
             <div className="w-full h-full ">
-                <div className="flex flex-col h-full justify-between items-center bg-darkRed p-4 text-cream rounded-xs">
-                    <p className="text-center">{data?.title}</p>
+                <div className="flex flex-col h-full justify-center items-center bg-darkRed p-4 text-cream rounded-xs">
                     <img src="/t4_card_bg.png" className="w-24" alt="Logo" />
                 </div>
             </div>
